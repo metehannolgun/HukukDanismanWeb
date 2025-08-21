05c4a16304d52cfda10916bfea938979cc646091
metehannolgun <metehannolgun@gmail.com>
2025-08-21 20:50:08 +0300

    Upload framer-motion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,30 @@ import ServicesPreview from '../sections/ServicesPreview'
 import ContactSection from '../sections/ContactSection'
 import { Element } from 'react-scroll'
 import { scroller } from 'react-scroll';
+import { motion } from "motion/react"
 
 const Home = () => {
   return (
    <div>
     <Element name="home">
-       <section  className=' bg-red-500 h-screen flex items-center justify-center relative  '>
-        HomePage
-        
-      </section>
+       <motion.section  className='bg-red-500 h-screen flex flex-col items-center justify-center relative
+        ' initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 3 }}  >
+        <span>HomePage</span>
+        <div className='flex flex-row items-center gap-2'>
+         <button
+          onClick={() => scroller.scrollTo('about', { smooth: true, duration: 800, offset: -0 })}
+          className="px-6 py-2 bg-blue-600 text-white rounded-full shadow hover:bg-blue-700 transition" 
+          >
+          Hakkımızda
+          </button>
+           <button
+          onClick={() => scroller.scrollTo('services', { smooth: true, duration: 800, offset: -0 })} 
+           className="px-6 py-2 bg-blue-600 text-white rounded-full shadow hover:bg-blue-700 transition">
+          Hakkımızda
+          </button>
+        </div>
+       
+      </motion.section>
     </Element>
     <Element name='about' >
         <section className='bg-blue-500 h-screen flex items-center justify-center relative'>
